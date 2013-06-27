ca5ac11ff38e54f5e508d22bdb7159f0e0854368
aesiu <siu.andrew123@gmail.com>
2013-06-27 19:23:59 -0400

    Update player.cpp

--- cardGameStraight/player.cpp
+++ cardGameStraight/player.cpp
@@ -1,19 +1,29 @@
 #include "player.h"
 
+Pile player::pile = *(new Pile());
 player::player(){
 	score = 0;
 	//hand = NULL;
 	//discard = NULL;
 }
 
-void player::dealNewHand(vector<Card>deck , int playerNum){
-	int counter = 0;
-	for(int i = 13 * (playerNum-1); i < 13 * (playerNum); i++ ){
-		hand[counter++] = deck[i];
-	}
+player::~player(){
+}
+
+void player::dealNewHand(vector<Card>deck){
+	hand = deck;
 }
 
 void player::discardCard(Card card){
+	bool legal = false;
+	for(vector<Card>::iterator i = hand.begin(); i != hand.end(); i++){
+		if (pile.isValidMove(*i)){
+			legal = true;
+		}
+	}
+	if(legal){
+		throw string("You have a legal play. You may not discard.");
+	}
 	discard.push_back(card);
 	score += card.getRank();
 	destroyCard(card);
@@ -25,6 +35,7 @@ void player::destroyCard(Card card){
 	for(vector<Card>::iterator i = hand.begin(); i != hand.end(); i++){
 		if(*i == card){
 			hand.erase(i);
+			return;
 		}
 	}
 }
@@ -33,6 +44,48 @@ void player::playCard(Card card){
 	if(pile.isValidMove(card)){
 		pile.addToPile(card);
 		destroyCard(card);
+	} else{
+		throw string("This is not a legal play.");
 	}
-	throw "This is not a legal play.";
+}
+
+string player::getRangeOfSuit(Suit suit){
+	return pile.getRangeOfSuit(suit);
+}
+string player::getHandString(){
+	string s ="";
+	for(vector<Card>::iterator i = hand.begin(); i != hand.end(); i++){
+		s+= i->convertRankToString() + i->convertSuitToString() + " ";
+	}
+	return s;
+}
+string player::getLegalPlaysString(){
+	string s ="";
+	for(vector<Card>::iterator i = hand.begin(); i != hand.end(); i++){
+		if (pile.isValidMove(*i)){
+			s+= i->convertRankToString() + i->convertSuitToString() + " ";
+		}
+	}
+	return s;
+}
+
+bool player::hasCard(Card card){
+	for(vector<Card>::iterator i = hand.begin(); i != hand.end(); i++){
+		if(*i == card){
+			return true;
+		}
+	}
+	return false;
+}
+int player::getScore(){
+	return score;
+}
+vector<Card> player::getHand(){
+	return hand;
+}
+vector<Card> player::getDiscard(){
+	return discard;
+}
+bool player::isHuman(){
+	return human;
 }
