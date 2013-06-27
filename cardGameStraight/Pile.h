b1bef408af2ed59b401e8d136ff2d01a65541896
aesiu <siu.andrew123@gmail.com>
2013-06-27 19:21:41 -0400

    Update Pile.h

--- cardGameStraight/Pile.h
+++ cardGameStraight/Pile.h
@@ -1,3 +1,7 @@
+#ifndef _PILE_
+#define _PILE_
+
+
 #include "Card.h"
 #include <vector>
 
@@ -7,8 +11,10 @@ public:
 	~Pile();
 	void addToPile(Card card);
 	bool isValidMove(Card card);
+	string getRangeOfSuit(Suit suit);
 private:
 	vector <Card> pile;
 	int validCards[8]; // first four are the smallest value of all suites, the next four are the biggest value
 };
 
+#endif
