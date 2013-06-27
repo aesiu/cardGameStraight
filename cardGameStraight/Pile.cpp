8fcb479db62ceea56617a7648177d2423bcec076
aesiu <siu.andrew123@gmail.com>
2013-06-27 19:21:02 -0400

    Update Pile.cpp

--- cardGameStraight/Pile.cpp
+++ cardGameStraight/Pile.cpp
@@ -1,12 +1,21 @@
-#include "Pile.h"
+#ifndef _PILE_
+#define _PILE_
+
+
+#include "Card.h"
+#include <vector>
+
+class Pile{#include "Pile.h"
 
 Pile::Pile(){
-	validCards[SPADE]  = SEVEN;
-	validCards[SPADE+4] = SEVEN;
+	for (int i =0; i < 8; i++){
+		validCards[i]  = -2;
+	}
+
 }
 
 Pile::~Pile(){
-
+	
 }
 
 void Pile::addToPile(Card card){
@@ -24,6 +33,35 @@ void Pile::addToPile(Card card){
 }
 
 bool Pile::isValidMove(Card card){
-	return (validCards[card.getSuit()] == card.getRank() -1 
-		|| validCards[card.getSuit()+4] == card.getRank()+1);
+	if(validCards[SPADE]==-2){
+		return (card.getRank() == SEVEN && card.getSuit() == SPADE);
+	}else if (card.getRank() == SEVEN)
+		return true;
+	return (validCards[card.getSuit()]-1 == card.getRank() 
+		|| validCards[card.getSuit()+4]+1 == card.getRank());
 }
+
+string Pile::getRangeOfSuit(Suit suit){
+	if (validCards[suit] == -2){
+		return "";
+	}
+	string ranks[RANK_COUNT] = {"A", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "J", "Q", "K"};
+	string s = "";
+	for (int i = validCards[suit]; i <= validCards[suit+4]; i++){
+		s += ranks[i]+" ";
+	}
+	return s;
+}
+public:
+	Pile();
+	~Pile();
+	void addToPile(Card card);
+	bool isValidMove(Card card);
+	string getRangeOfSuit(Suit suit);
+private:
+	vector <Card> pile;
+	int validCards[8]; // first four are the smallest value of all suites, the next four are the biggest value
+};
+
+#endif
