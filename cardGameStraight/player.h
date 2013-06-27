2e54d4c473f528a85d4b25c847baf4b1e987c847
aesiu <siu.andrew123@gmail.com>
2013-06-27 19:24:52 -0400

    Update player.h

--- cardGameStraight/player.h
+++ cardGameStraight/player.h
@@ -1,3 +1,7 @@
+#ifndef _PLAYER_
+#define _PLAYER_
+
+
 #include "Pile.h"
 #include "Card.h"
 #include <vector>
@@ -7,15 +11,26 @@ class player{
 public:
     player();
 	~player();
-	void dealNewHand(vector<Card>deck,  int playerNum);
+	void dealNewHand(vector<Card>deck);
 	virtual void discardCard(Card card);
 	void playCard(Card card);
+	virtual string autoplay() = 0;
+	static string getRangeOfSuit(Suit);
+	string getHandString();
+	string getLegalPlaysString();
+	int getScore();
+	vector<Card> getHand();
+	vector<Card> getDiscard();
+	bool isHuman();
+	bool hasCard(Card card);
 protected:
 	int score;
 	vector<Card> hand;
 	vector<Card> discard;
 	static Pile pile;
+	bool human;
 private:
 	void destroyCard(Card card);
 };
 
+#endif
